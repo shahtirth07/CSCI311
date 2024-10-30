e8f8376cbb8b79762d3f162950df3d470784ce63
Tirth Bhavesh Shah <tshah@csuchico.edu>
2024-10-30 16:25:17 -0700

    added code for Merge Sort

--- Sorting.cpp
+++ Sorting.cpp
@@ -79,6 +79,66 @@ void selectionSort(int arr[], int size)
     }
 }
 
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        // int m = (l + r) / 2; might piss of interviewer overflow and int
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+void merge(int arr[], int l, int m, int n)
+{
+    int n1 = m - l + 1;
+    int n2 = n - m;
+    int L[n1], R[n2];
+    int i = 0;
+    while (i < n1)
+    {
+        L[i] = arr[l + i];
+        i++;
+    }
+    int j = 0;
+    while (j < n2)
+    {
+        R[j] = arr[m + 1 + j];
+        j++;
+    }
+    i = 0;
+    j = 0;
+    int k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
 int main()
 {
     int arr[] = {64, 34, 25, 12, 11, 90, 1};
