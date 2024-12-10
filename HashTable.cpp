7d33eb96cea2b405a2dd586ceca7d71561ca207f
Tirth Bhavesh Shah <tshah@csuchico.edu>
2024-12-09 16:22:49 -0800

    Deleting Quadratic Probing

--- HashTable.cpp
+++ HashTable.cpp
@@ -84,6 +84,27 @@ public:
         } while (index != originalIndex && i < capacity);
         return false;
     }
+    bool deleteQuadraticProbing(int key)
+    {
+        int index = hash(key);
+        int originalIndex = index;
+        int i = 1;
+        do
+        {
+            int newIndex = (index + i * i) % capacity;
+            if (table[newIndex] == -1)
+            {
+                return false;
+            }
+            else if (table[newIndex] == key)
+            {
+                table[newIndex] = -2;
+                return true;
+            }
+            i++;
+        } while (index != originalIndex && i < capacity);
+        return false;
+    }
 
     void print()
     {
@@ -97,13 +118,33 @@ public:
 
 int main()
 {
-    HashTable ht(10);
-    ht.insertLinearProbing(10);
-    ht.insertLinearProbing(20);
-    ht.insertLinearProbing(30);
-    ht.insertLinearProbing(40);
-    ht.insertLinearProbing(50);
-    ht.deleteLinearProbing(20);
+    HashTable ht1(11);
+    ht1.insertLinearProbing(10);
+    ht1.insertLinearProbing(20);
+    ht1.insertLinearProbing(30);
+    ht1.insertLinearProbing(40);
+    ht1.insertLinearProbing(50);
+    ht1.insertLinearProbing(60);
+    ht1.insertLinearProbing(70);
+    ht1.insertLinearProbing(80);
+    ht1.insertLinearProbing(90);
+    ht1.insertLinearProbing(100);
+    ht1.print();
+    ht1.deleteLinearProbing(20);
+    ht1.print();
+    HashTable ht(11);
+    ht.insertQuadraticProbing(10);
+    ht.insertQuadraticProbing(20);
+    ht.insertQuadraticProbing(30);
+    ht.insertQuadraticProbing(40);
+    ht.insertQuadraticProbing(50);
+    ht.insertQuadraticProbing(60);
+    ht.insertQuadraticProbing(70);
+    ht.insertQuadraticProbing(80);
+    ht.insertQuadraticProbing(90);
+    ht.insertQuadraticProbing(100);
+    ht.print();
+    ht.deleteQuadraticProbing(20);
     ht.print();
     return 0;
 }
